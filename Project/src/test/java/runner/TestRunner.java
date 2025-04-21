1362862ccff79afeb812a5da2ab996ed237d919c
prakash-avvaru <prakashavvaru7@gmail.com>
2025-04-21 12:32:52 +0000

    prakash commit

--- Project/src/test/java/runner/TestRunner.java
+++ Project/src/test/java/runner/TestRunner.java
@@ -1,5 +1,31 @@
 package runner;
 
-public class TestRunner {
-    
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import utils.Base;
+
+public class TestRunner extends Base{
+    @BeforeClass
+    public void start(){
+
+    }
+    @BeforeMethod
+    public void launch() throws IOException{
+        openBrowser();
+    }
+    @Test
+    @AfterMethod
+    public void tear(){
+        driver.quit();
+    }
+    @AfterClass
+    public void end(){
+
+    }
 }
--- Project/src/test/java/utils/LoggerHandler.java
+++ Project/src/test/java/utils/LoggerHandler.java
@@ -1,5 +1,40 @@
 package utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
 public class LoggerHandler {
+    private static final Logger logger=Logger.getLogger(LoggerHandler.class);
+    static{
+        File logs=new File("logs");
+        String logFile=String.format("logs/logfile_%s.log",new
+        SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
+        try {
+            logger.addAppender(new FileAppender(new PatternLayout("%d{ISO8601} %-5p %c-%m%n"),logFile,true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void info(String message){
+        logger.info(message);
+    }
+    public static void debug(String message){
+        logger.info(message);
+    }
+    public static void warn(String message){
+        logger.info(message);
+    }
+    public static void error(String message){
+        logger.info(message);
+    }
+    public static void fatal(String message){
+        logger.info(message);
+    }
     
 }
