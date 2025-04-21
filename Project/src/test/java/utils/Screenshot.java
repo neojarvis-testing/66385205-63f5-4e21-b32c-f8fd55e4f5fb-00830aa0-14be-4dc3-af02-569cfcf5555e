88ba285477a18fc178c6b62c990ee0a59c6de6cb
Suhana-007 <suhanamoulali@gmail.com>
2025-04-21 12:05:41 +0000

    suhana commit

--- Project/src/test/java/utils/Screenshot.java
+++ Project/src/test/java/utils/Screenshot.java
@@ -1,5 +1,33 @@
 package utils;
 
-public class Screenshot {
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.google.common.io.Files;
+
+public class Screenshot extends Base{
+    public static TakesScreenshot ss;
     
+    public static void getScreenshot(String filename){
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String name = filename + timestamp + ".png";
+        ss = (TakesScreenshot)driver;
+        File sourceFile = ss.getScreenshotAs(OutputType.FILE);
+
+        File screenshotsDirectory = new File(System.getProperty("user.dir")+"/screenshots");
+        if(!screenshotsDirectory.exists()){
+            screenshotsDirectory.mkdirs();
+        }
+
+        File destinationFile = new File(screenshotsDirectory, name);
+        try {
+            Files.copy(sourceFile, destinationFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
